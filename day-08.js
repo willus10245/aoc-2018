cf4df6674f5072e93f1f6f5daad36ef675450c8f
Scott Wiggins <dscottwiggins@gmail.com>
2018-12-08 16:07:44 -0500

    day 8

---
+++ day-08.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const fileName = process.argv[2]
+
+function buildTree(numOfChildNodes, numOfMetadata, rest) {
+  if (numOfChildNodes === 0) {
+    const metadata = rest.slice(0, numOfMetadata)
+    const value = metadata.reduce((acc, curr) => acc + curr, 0)
+    console.log('leaf node value: ', value)
+    return [value, rest.slice(numOfMetadata)]
+  } else {
+    let childNodesLeft = numOfChildNodes
+    let currRest = rest
+    let childrensValues = []
+    while (childNodesLeft > 0) {
+      const [first, second, ...newRest] = currRest
+      const arr = buildTree(first, second, newRest)
+      childrensValues = childrensValues.concat(arr[0])
+      currRest = arr[1]
+      childNodesLeft--
+      console.log('children nodes values: ', childrensValues)
+    }
+    const metadata = currRest.slice(0, numOfMetadata)
+    currRest = currRest.slice(numOfMetadata)
+    const value = metadata.reduce((acc, curr) => {
+      if (curr === 0 || !childrensValues[curr - 1]) return acc
+      return acc + childrensValues[curr - 1]
+    }, 0)
+    console.log('node with children value: ', value)
+    return [value, currRest]
+  }
+}
+
+fs.readFile(fileName, 'utf8', (err, data) => {
+  if (err) throw err
+  const [first, second, ...rest] = data.split(' ').map(Number)
+  const valueArray = buildTree(first, second, rest)
+  console.log(valueArray[0])
+})
